50746ce748498a0ad2a3c91e85464610ebea9250
Ian Saultz <52051793+atierian@users.noreply.github.com>
2021-12-07 09:07:39 -0500

    Day 7

---
+++ AdventOfCode_2021.playground/Pages/Day 7.xcplaygroundpage/Contents.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+// MARK: Input
+let positions = DaySeven
+    .input
+    .components(separatedBy: ",")
+    .compactMap(Int.init)
+    .sorted()
+
+
+// MARK: Part One
+
+let median = positions[positions.count/2]
+
+func fuelConsumption(_ median: Int) -> Reducing<UInt, Int> {
+    Reducing { $0 + $1.distance(to: median).magnitude }
+}
+
+let partOne = positions
+    .reduce(0, fuelConsumption(median))
+
+
+// MARK: Part Two
+
+let mean = positions
+    .reduce(0, +) / positions.count
+
+func incrementalFuelConsumption(mean: Int) -> Reducing<Int, Int> {
+    Reducing {
+        let d = $1.distance(to: mean)
+        return $0 + d * (d + 1) / 2
+    }
+}
+
+let partTwo = positions
+    .reduce(0, incrementalFuelConsumption(mean: mean))
+
+
+
+
